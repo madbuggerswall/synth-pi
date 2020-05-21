c9157a1a63c44f4e954d1701b2df1d0ff7c50d0d
Ege Furkan Bilgin <furkanegebilgin@gmail.com>
2020-05-21 22:17:02 +0300

    First commit.

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include "../WiringPi/wiringPi/wiringPi.h"
+// #include <wiringPi.h>
+#include <cmath>
+#include <iostream>
+
+class Synthesizer {
+	static const int sampleRate = 48000;
+};
+
+class Oscillator {
+};
+
+int main() {
+	const int pwmPin = 1;
+	const int freqA = 440;
+	const int delay = 20;
+	const float pi = std::acos(-1);
+
+	float phase;
+	wiringPiSetup();
+	pinMode(pwmPin, PWM_OUTPUT);
+
+	while (true) {
+		pwmWrite(pwmPin, std::sin(phase));
+		phase += freqA * 2.0f * pi / 50000.0f;
+		std::cout << phase << std::endl;
+		delayMicroseconds(delay);
+	}
+
+	return 0;
+}
